a4a206a993aa49c7d835f9f946999fa7246e5893
Harikrishnan B A <harikrishnanasok@gmail.com>
2023-11-14 20:51:39 +0530

    Basic styling

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,24 @@
-import './App.css'
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Product from "./pages/Product";
+import Pricing from "./pages/Pricing";
+import HomePage from "./pages/HomePage";
+import PageNotFound from "./pages/PageNotFound";
+import AppLayout from "./pages/AppLayout";
+import Login from "./pages/Login";
 
 function App() {
   return (
-    <div>
-      <h1>WorldWise</h1>
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/product" element={<Product />} />
+        <Route path="/pricing" element={<Pricing />} />
+        <Route path="/app" element={<AppLayout/>}/>
+        <Route path="/login" element={<Login/>}/>
+        <Route path="*" element={<PageNotFound/>}/>
+      </Routes>
+    </BrowserRouter>
   );
 }
 
-export default App
+export default App;
